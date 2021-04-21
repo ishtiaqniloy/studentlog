9ffa92f46ad535de429ce4561c816e2aad44c035
Ishmum Jawad Khan <ishmumkhan@gmail.com>
2021-04-21 22:05:07 +0600

    [bug-fix] spring test properties location

--- src/test/java/com/ideal/studentlog/controllers/StudentControllerTest.java
+++ src/test/java/com/ideal/studentlog/controllers/StudentControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,6 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class StudentControllerTest {
 
     @Autowired
