13807efe17a90ac9a7602ed5736eda4e253e503e
Ishmum Jawad Khan <ishmumkhan@gmail.com>
2021-04-22 09:54:28 +0600

    [config] default test properties

--- src/test/java/com/ideal/studentlog/controllers/StudentControllerTest.java
+++ src/test/java/com/ideal/studentlog/controllers/StudentControllerTest.java
@@ -5,13 +5,12 @@ import com.ideal.studentlog.database.repositories.StudentRepository;
 import com.ideal.studentlog.helpers.dtos.StudentDTO;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,7 +28,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
 public class StudentControllerTest {
 
     @Autowired
