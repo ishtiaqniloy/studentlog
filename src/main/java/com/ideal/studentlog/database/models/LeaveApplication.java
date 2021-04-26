b1db679cefc3cd1c697ca84ca406979b13abca53
nazmussaif-dsi <nazmussaif-dsi@dsinnovators.com>
2021-04-26 17:46:06 +0600

    Adds feature: Leave Application

---
+++ src/main/java/com/ideal/studentlog/database/models/LeaveApplication.java
@@ -0,0 +1,35 @@
+package com.ideal.studentlog.database.models;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.NonNull;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class LeaveApplication {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @NonNull
+    Date dateFrom;
+
+    @NonNull
+    Date dateTo;
+
+    @NonNull
+    @ManyToOne
+    Student student;
+
+    @NonNull
+    String applicationBody;
+
+    @ManyToOne
+    Teacher approvedBy;
+}
