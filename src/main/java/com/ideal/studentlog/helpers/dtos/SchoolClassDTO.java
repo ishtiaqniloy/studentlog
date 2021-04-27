8ccc00bdcb69b1d9025adb51ad563480c64cd9d5
Abdullah Al Ishtiaq <abdullah.ishtiaq@dsinnovators.com>
2021-04-27 09:22:29 +0600

    add validation in SchoolClassDTO

--- src/main/java/com/ideal/studentlog/helpers/dtos/SchoolClassDTO.java
+++ src/main/java/com/ideal/studentlog/helpers/dtos/SchoolClassDTO.java
@@ -4,10 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NonNull;
 
+import javax.validation.constraints.Size;
+
 @Data
 @AllArgsConstructor
 public class SchoolClassDTO {
     @NonNull
+    @Size(min = 1, max = 20)
     String name;
 
     @NonNull
