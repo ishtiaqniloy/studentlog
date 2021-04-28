fe614869c0eb5f33c54a5e021d8b262e1010c49b
lubna40 <lubu7035@gmail.com>
2021-04-29 03:54:00 +0600

    Add mapstruct for Class Details

--- src/main/java/com/ideal/studentlog/services/ClassDetailsService.java
+++ src/main/java/com/ideal/studentlog/services/ClassDetailsService.java
@@ -1,11 +1,15 @@
 package com.ideal.studentlog.services;
 
 import com.ideal.studentlog.database.models.ClassDetails;
+import com.ideal.studentlog.database.models.SchoolClass;
+import com.ideal.studentlog.database.models.Teacher;
 import com.ideal.studentlog.database.repositories.ClassDetailsRepository;
 import com.ideal.studentlog.database.repositories.SchoolClassRepository;
 import com.ideal.studentlog.database.repositories.TeacherRepository;
 import com.ideal.studentlog.helpers.dtos.ClassDetailsDTO;
 import com.ideal.studentlog.helpers.exceptions.ServiceException;
+import com.ideal.studentlog.helpers.mappers.ClassDetailsMapper;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -16,72 +20,68 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class ClassDetailsService {
+    private static final ClassDetailsMapper mapper = ClassDetailsMapper.INSTANCE;
+
     private final ClassDetailsRepository repository;
     private final SchoolClassRepository schoolClassRepository;
     private final TeacherRepository teacherRepository;
 
-    //Todo: introduce model mapper
-    public ClassDetailsDTO map(ClassDetails classDetails){
-        return new ClassDetailsDTO(
-                classDetails.getSchoolClass().getId(),
-                classDetails.getYear(),
-                classDetails.getTeacher().getId()
-        );
-    }
-
-    public void map(ClassDetails classDetails, ClassDetailsDTO classDetailsDTO) throws ServiceException {
-        classDetails.setSchoolClass(schoolClassRepository.findById(classDetailsDTO.getSchoolClassId()).orElseThrow(
-                () -> new ServiceException(
-                "School Class not found with ID: " + classDetailsDTO.getSchoolClassId(),
-                HttpStatus.NOT_FOUND
-        )));
-        classDetails.setYear(classDetailsDTO.getYear());
-        classDetails.setTeacher(teacherRepository.findById(classDetailsDTO.getTeacherId()).orElseThrow(
-                () -> new ServiceException(
-                "Teacher not found with ID: " + classDetailsDTO.getTeacherId(),
-                HttpStatus.NOT_FOUND
-        )));
-    }
-
-    public List<ClassDetailsDTO> map(List<ClassDetails> classDetailsList){
-        return classDetailsList
+    public List<ClassDetailsDTO> getAll(){
+        return repository
+                .findAll()
                 .stream()
-                .map(this::map)
+                .map(mapper::classDetailsToClassDetailsDto)
                 .collect(Collectors.toList());
     }
 
-    public ClassDetails createModelWithDTO(ClassDetailsDTO classDetailsDTO) throws ServiceException {
-        ClassDetails classDetails = new ClassDetails();
-        map(classDetails, classDetailsDTO);
-        return classDetails;
-    }
-
-    public List<ClassDetailsDTO> getAll(){
-        return map(repository.findAll());
-    }
-
     public ClassDetailsDTO getById(Integer id) throws ServiceException {
-        ClassDetails classDetails = repository.findById(id).orElseThrow(() -> new ServiceException(
-                        "Class Details not found with ID: " + id,
-                        HttpStatus.NOT_FOUND
-                ));
-        return map(classDetails);
+        return mapper.classDetailsToClassDetailsDto(getClassDetails(id));
     }
 
-    public ClassDetailsDTO create(ClassDetailsDTO classDetailsDTO) throws ServiceException {
-        return map(repository.save(createModelWithDTO(classDetailsDTO)));
+    public ClassDetailsDTO create(ClassDetailsDTO dto) throws ServiceException {
+        ClassDetails classDetails = new ClassDetails();
+
+        mapper.classDetailsDtoToClassDetails(dto, classDetails);
+        classDetails.setSchoolClass(getSchoolClass(dto.getSchoolClassId()));
+        classDetails.setTeacher(getTeacher(dto.getTeacherId()));
+
+        return mapper.classDetailsToClassDetailsDto(repository.save(classDetails));
     }
 
-    public ClassDetailsDTO update(Integer id, ClassDetailsDTO classDetailsDTO) throws ServiceException {
-        ClassDetails classDetails = repository.findById(id).orElseThrow(() -> new ServiceException(
-                "Class Details not found with ID: " + id,
-                HttpStatus.NOT_FOUND
-        ));
-        map(classDetails, classDetailsDTO);
-        return map(repository.save(classDetails));
+    public ClassDetailsDTO update(Integer id, ClassDetailsDTO dto) throws ServiceException {
+        ClassDetails classDetails = getClassDetails(id);
+
+        mapper.classDetailsDtoToClassDetails(dto, classDetails);
+        classDetails.setSchoolClass(getSchoolClass(dto.getSchoolClassId()));
+        classDetails.setTeacher(getTeacher(dto.getTeacherId()));
+
+        return mapper.classDetailsToClassDetailsDto(repository.save(classDetails));
     }
 
     public void delete(Integer id) {
         repository.deleteById(id);
+    }
+
+    private ClassDetails getClassDetails(Integer id) throws ServiceException {
+        return repository.findById(id).orElseThrow(() -> new ServiceException(
+                "Class Details not found with ID: " + id,
+                HttpStatus.NOT_FOUND
+        ));
+    }
+
+    private SchoolClass getSchoolClass(@NonNull Integer id) throws ServiceException {
+        return schoolClassRepository.findById(id)
+                .orElseThrow(() -> new ServiceException(
+                        "School Class not found with ID: " + id,
+                        HttpStatus.NOT_FOUND
+                ));
+    }
+
+    private Teacher getTeacher(@NonNull Integer id) throws ServiceException {
+        return teacherRepository.findById(id)
+                .orElseThrow(() -> new ServiceException(
+                        "Teacher not found with ID: " + id,
+                        HttpStatus.NOT_FOUND
+                ));
     }
 }
